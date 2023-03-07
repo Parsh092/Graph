49c2ece3cdc2fedc3bdc841c4d2f4d68c6c6dbcb
Parshva Choradia <112927478+Parsh092@users.noreply.github.com>
2023-03-07 09:37:59 +0530

    Update and rename DisjointByUnion.java to Leetcode.java

--- src/Leetcode_Codechef/Leetcode.java
+++ src/Leetcode_Codechef/Leetcode.java
@@ -1,6 +1,6 @@
 //import java.util.*;
 //
-//public class Graph {
+//public class Leetcode {
 //    public static void main(String[] args) {
 //       Scanner sc = new Scanner(System.in);
 //       String pattern = sc.next();
